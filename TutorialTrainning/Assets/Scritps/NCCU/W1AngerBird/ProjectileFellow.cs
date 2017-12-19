14083eba314626965321d22370435963f6e64492
MythZ <zen1225@gmail.com>
2017-12-20 02:05:27 +0800

    [MZ]Add

---
+++ TutorialTrainning/Assets/Scritps/NCCU/W1AngerBird/ProjectileFellow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileFellow : MonoBehaviour {
+
+
+    public Transform Projectile;
+    public Transform farLeft;
+    public Transform farRight;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPosition = transform.position;
+        newPosition.x = Projectile.position.x;
+        newPosition.x = Mathf.Clamp(newPosition.x, farLeft.position.x, farRight.position.x);
+        transform.position = newPosition;
+
+	}
+}
